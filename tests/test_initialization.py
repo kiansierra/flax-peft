761cd4b248e94fc8caef20c6fef2ecf85c45a6b2
kiansierra <kiansierra90@gmail.com>
2023-12-05 12:23:23 +0100

    Change to FlaxBertModel with all layers initialized

--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -1,15 +1,16 @@
 import flax
 import jax
 import jax.numpy as jnp
-from flax_lora import LoraConfig, LoraWrapper, build_lora_model
 from transformers import BertTokenizerFast, FlaxBertForMaskedLM, FlaxBertModel
+
+from flax_lora import LoraConfig, LoraWrapper, build_lora_model
 
 
 def test_bert() -> None:
     key = jax.random.PRNGKey(0)
-    model = FlaxBertForMaskedLM.from_pretrained('prajjwal1/bert-mini', revision='refs/pr/3')
+    model = FlaxBertModel.from_pretrained('prajjwal1/bert-mini', revision='refs/pr/3')
     module, base_params = model.module, model.params
-    lora_config = LoraConfig(rank=16, lora_alpha=2, target_modules=['word_embedding'])
+    lora_config = LoraConfig(rank=16, lora_alpha=2, target_modules=['word_embeddings'])
     lora_module = build_lora_model(module, lora_config, base_params)
     inputs = dict(input_ids=jnp.ones((1, 1), dtype=jnp.int32),
                   attention_mask=jnp.ones((1, 1), dtype=jnp.int32),
@@ -17,12 +18,12 @@ def test_bert() -> None:
     lora_params = lora_module.init(key, base_params, **inputs)
     model_outputs = module.apply({'params': base_params}, **inputs)
     lora_outputs = lora_module.apply(lora_params, base_params, **inputs)
-    assert jnp.allclose(model_outputs.logits, lora_outputs.logits, atol=1e-8), "Lora outputs should be close to the base model outputs"
+    assert jnp.allclose(model_outputs.last_hidden_state, lora_outputs.last_hidden_state, atol=1e-8), "Lora outputs should be close to the base model outputs"
     
     
 def test_bert_targets() -> None:
     key = jax.random.PRNGKey(0)
-    model = FlaxBertForMaskedLM.from_pretrained('prajjwal1/bert-mini', revision='refs/pr/3')
+    model = FlaxBertModel.from_pretrained('prajjwal1/bert-mini', revision='refs/pr/3')
     module, base_params = model.module, model.params
     lora_config = LoraConfig(rank=16, lora_alpha=2, target_modules=['word_embeddings'])
     lora_module = build_lora_model(module, lora_config, base_params)
@@ -37,7 +38,7 @@ def test_bert_targets() -> None:
     
 def test_bert_lora_mappings_embeddings() -> None:
     key = jax.random.PRNGKey(0)
-    model = FlaxBertForMaskedLM.from_pretrained('prajjwal1/bert-mini', revision='refs/pr/3')
+    model = FlaxBertModel.from_pretrained('prajjwal1/bert-mini', revision='refs/pr/3')
     module, base_params = model.module, model.params
     lora_config = LoraConfig(rank=16, lora_alpha=2, target_modules=['word_embeddings'])
     lora_module = build_lora_model(module, lora_config, base_params)
@@ -51,7 +52,7 @@ def test_bert_lora_mappings_embeddings() -> None:
     
 def test_bert_lora_mappings_qk() -> None:
     key = jax.random.PRNGKey(0)
-    model = FlaxBertForMaskedLM.from_pretrained('prajjwal1/bert-mini', revision='refs/pr/3')
+    model = FlaxBertModel.from_pretrained('prajjwal1/bert-mini', revision='refs/pr/3')
     module, base_params = model.module, model.params
     lora_config = LoraConfig(rank=16, lora_alpha=2, target_modules=['query', "key"])
     lora_module = build_lora_model(module, lora_config, base_params)
