4b9332d7f933ce544c24fee9f06fe52994de9c8a
kiansierra <kiansierra90@gmail.com>
2023-12-09 05:39:01 +0100

    added pefttype and task type

--- src/flax_lora/utils.py
+++ src/flax_lora/utils.py
@@ -6,6 +6,36 @@ import flax.linen as nn
 import jax
 from flax.core.frozen_dict import FrozenDict
 
+import enum
+
+CONFIG_NAME = "adapter_config.json"
+
+class PeftType(str, enum.Enum):
+    """Enum class for the different types of adapters in PEFT."""
+
+    PROMPT_TUNING = "PROMPT_TUNING"
+    MULTITASK_PROMPT_TUNING = "MULTITASK_PROMPT_TUNING"
+    P_TUNING = "P_TUNING"
+    PREFIX_TUNING = "PREFIX_TUNING"
+    LORA = "LORA"
+    ADALORA = "ADALORA"
+    ADAPTION_PROMPT = "ADAPTION_PROMPT"
+    IA3 = "IA3"
+    LOHA = "LOHA"
+    LOKR = "LOKR"
+    OFT = "OFT"
+
+
+class TaskType(str, enum.Enum):
+    """Enum class for the different types of tasks supported by PEFT."""
+
+    SEQ_CLS = "SEQ_CLS"
+    SEQ_2_SEQ_LM = "SEQ_2_SEQ_LM"
+    CAUSAL_LM = "CAUSAL_LM"
+    TOKEN_CLS = "TOKEN_CLS"
+    QUESTION_ANS = "QUESTION_ANS"
+    FEATURE_EXTRACTION = "FEATURE_EXTRACTION"
+
 
 def merge_lora_params(base_params: dict | FrozenDict, lora_update_params: dict | FrozenDict) -> dict | FrozenDict:
     """
